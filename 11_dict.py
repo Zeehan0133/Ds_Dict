78bedbfd694b36364775272542bdbfe5b5f7ebbe
Zeehan0133 <zyne0133@gmail.com>
2021-11-28 00:31:49 +0530

    [Zeehan] Add:add key in a dict

---
+++ 11_dict.py
@@ -0,0 +1,10 @@
+"""
+@Author: Zeehan 
+@Date: 2021-26-11 19:00:30
+@Title :   Python script to add a key to a dictionary.
+"""
+mydict={}
+mydict["python"]=100
+mydict["cpp"]=50
+mydict["c"]=20
+print(mydict)
